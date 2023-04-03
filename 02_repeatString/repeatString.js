ac5909dfeffd0ef57fb208f3acfad170bfc47afa
E.J. Perez <ejperez37@gmail.com>
2023-04-03 13:10:12 -0400

    Complete Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let newString = ''
+    for(let i = 0; i < number; i++) {
+        newString += string;
+    }
+    
+    if(number < 0) {
+        return 'ERROR';
+    }
+    else return newString;
 };
 
 // Do not edit below this line
