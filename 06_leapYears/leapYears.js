66467efed9009ecb702d078e008f043bd129d8a9
E.J. Perez <ejperez37@gmail.com>
2023-04-03 13:55:48 -0400

    Complete Exercise 07 & remove unneccessary comments in Exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,4 @@
 const leapYears = function(year) {
-//create a function that takes a year as an argument and returns true if it is a leap year and false if it is not, a leap year is any year divisible by 4 but not by 100 unless it is also divisible by 400
     if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
         return true;
     }
