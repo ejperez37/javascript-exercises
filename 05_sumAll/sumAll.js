0f2e614fbde5691c6f8ec39861c0c9903854f4ce
E.J. Perez <ejperez37@gmail.com>
2023-04-03 13:41:33 -0400

    Complete Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(numAlpha, numBeta) {
 
+let finalSum = 0;
+for(let i = Math.min(numAlpha, numBeta); i <= Math.max(numAlpha, numBeta); i++){
+  finalSum += i;
+}
+
+if(numAlpha < 0 || numBeta < 0 || typeof numAlpha !== 'number' || typeof numBeta !== 'number'){
+  return 'ERROR';
+}
+else return finalSum;
 };
 
 // Do not edit below this line
